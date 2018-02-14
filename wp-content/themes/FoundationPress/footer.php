90a57cd534e42f867f76245c4e4b76f04cebbcd9
rmilano24 <rmilano24@gmail.com>
2018-02-13 17:49:36 -0800

    style up footer

--- wp-content/themes/FoundationPress/footer.php
+++ wp-content/themes/FoundationPress/footer.php
@@ -16,13 +16,64 @@
                     <img src="<?php echo get_stylesheet_directory_uri(); ?>/src/assets/images/pbfa_member_badge.png" alt="">
                 </div>
                 <div class="cell small-4">
-                    <a href="">info@tasterevit.com</a>
+                    <a href="" class="emailus">info@tasterevit.com</a>
                 </div>
                 <div class="cell small-4">
                     <ul>
-                        <li><a href=""></a></li>
-                        <li><a href=""></a></li>
-                        <li><a href=""></a></li>
+                        <li><a href="">
+                        <svg width="68px" height="62px" viewBox="0 0 68 62" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                            <g id="Final" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                <g id="Home-Revised-Final" transform="translate(-1108.000000, -4698.000000)">
+                                    <g id="Group-38" transform="translate(0.000000, 3071.000000)">
+                                        <g id="Group-28" transform="translate(0.000000, 1545.000000)">
+                                            <g id="Group-5" transform="translate(1108.000000, 82.000000)">
+                                                <ellipse id="Oval-6" fill="#787878" cx="33.7192982" cy="31" rx="33.7192982" ry="31"></ellipse>
+                                                <text id="" font-family="FontAwesome" font-size="30" font-weight="normal" line-spacing="23" fill="#222222">
+                                                    <tspan x="25" y="44"></tspan>
+                                                </text>
+                                            </g>
+                                        </g>
+                                    </g>
+                                </g>
+                            </g>
+                        </svg>
+                        </a></li>
+                        <li><a href="">                           
+                        <svg width="62px" height="62px" viewBox="0 0 62 62" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                            <g id="Final" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                <g id="Home-Revised-Final" transform="translate(-1196.000000, -4698.000000)">
+                                    <g id="Group-38" transform="translate(0.000000, 3071.000000)">
+                                        <g id="Group-28" transform="translate(0.000000, 1545.000000)">
+                                            <g id="Group-7" transform="translate(1196.000000, 82.000000)">
+                                                <circle id="Oval-6-Copy" fill="#787878" cx="31" cy="31" r="31"></circle>
+                                                <text id="" font-family="FontAwesome" font-size="30" font-weight="normal" line-spacing="23" fill="#222222">
+                                                    <tspan x="18" y="43"></tspan>
+                                                </text>
+                                            </g>
+                                        </g>
+                                    </g>
+                                </g>
+                            </g>
+                        </svg> 
+                        </a></li>
+                        <li><a href="">
+                        <svg width="62px" height="62px" viewBox="0 0 62 62" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+                            <g id="Final" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                                <g id="Home-Revised-Final" transform="translate(-1278.000000, -4698.000000)">
+                                    <g id="Group-38" transform="translate(0.000000, 3071.000000)">
+                                        <g id="Group-28" transform="translate(0.000000, 1545.000000)">
+                                            <g id="Group-19" transform="translate(1278.000000, 82.000000)">
+                                                <circle id="Oval-6-Copy-2" fill="#787878" cx="31" cy="31" r="31"></circle>
+                                                <text id="" font-family="FontAwesome" font-size="30" font-weight="normal" line-spacing="23" fill="#222222">
+                                                    <tspan x="17" y="44"></tspan>
+                                                </text>
+                                            </g>
+                                        </g>
+                                    </g>
+                                </g>
+                            </g>
+                        </svg>
+                        </a></li>
                     </ul>
                 </div>
             </div>
