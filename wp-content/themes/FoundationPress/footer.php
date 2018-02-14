ee597985639d774d1c7903dc28178a190dd41c85
rmilano24 <rmilano24@gmail.com>
2018-02-14 11:18:37 -0800

    get offcanvas menu working

--- wp-content/themes/FoundationPress/footer.php
+++ wp-content/themes/FoundationPress/footer.php
@@ -90,6 +90,7 @@
         </div>    
 </footer> 
 
+
 <?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) === 'offcanvas' ) : ?>
 	</div><!-- Close off-canvas content -->
 <?php endif; ?>
--- wp-content/themes/FoundationPress/header.php
+++ wp-content/themes/FoundationPress/header.php
@@ -21,9 +21,11 @@
 	</head>
 	<body <?php body_class(); ?>>
 
-	<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) === 'offcanvas' ) : ?>
+<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) === 'offcanvas' ) : ?>
 		<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
-	<?php endif; ?>
+<?php endif; ?>
+
+
 
 
 <header class="site-header" role="banner">
@@ -31,16 +33,14 @@
 			<nav class="site-navigation top-bar" role="navigation">
 				<div class="top-bar-left">
 					<div class="site-desktop-title top-bar-title">
+					<button aria-label="<?php _e( 'Main Menu', 'foundationpress' ); ?>" class="menu-icon" type="button" data-toggle="<?php foundationpress_mobile_menu_id(); ?>"></button>
 					<a href="/"><img src="<?php echo get_stylesheet_directory_uri(); ?>/src/assets/images/revit-logo.png" alt="Revit"></a>
 					</div>
 				</div>
-				<div class="top-bar-right">
+				<div class="top-bar-left">
 					<?php foundationpress_top_bar_r(); ?>
-	
-					<!-- <?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) === 'topbar' ) : ?>
-						<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
-					<?php endif; ?> -->
 				</div>
 			</nav>
 	</div>
 </header>
+
